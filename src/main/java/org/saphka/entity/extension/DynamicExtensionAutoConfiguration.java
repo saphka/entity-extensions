8e0b953a9dba4960bc987670295c9d046541b0ea
saphka <alex.loginov@pm.me>
2020-02-14 10:56:41 +0300

    Liquibase properties

--- src/main/java/org/saphka/entity/extension/DynamicExtensionAutoConfiguration.java
+++ src/main/java/org/saphka/entity/extension/DynamicExtensionAutoConfiguration.java
@@ -3,9 +3,12 @@ package org.saphka.entity.extension;
 import liquibase.integration.spring.SpringLiquibase;
 import org.saphka.entity.extension.liquibase.ExtensionCapableSpringLiquibase;
 import org.saphka.entity.extension.service.storage.CurrentConfigurationReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -22,15 +25,42 @@ import javax.sql.DataSource;
 @EntityScan
 public class DynamicExtensionAutoConfiguration {
 
-	@Bean
-	@ConditionalOnProperty(name = "entity.extension.liquibase.enabled", havingValue = "true", matchIfMissing = true)
-	public SpringLiquibase liquibase(DataSource dataSource, @Value("${spring.liquibase.change-log}") String changeLog, CurrentConfigurationReader currentConfigurationReader) {
-		ExtensionCapableSpringLiquibase liquibase = new ExtensionCapableSpringLiquibase();
-		liquibase.setChangeLog(changeLog);
-		liquibase.setDataSource(dataSource);
-		liquibase.setExtensions(currentConfigurationReader.getCurrentExtensions());
+	@Configuration
+	@EnableConfigurationProperties(LiquibaseProperties.class)
+	@ConditionalOnProperty(prefix = "entity.extension.liquibase", name = "enabled", havingValue = "true", matchIfMissing = true)
+	public static class DynamicExtensionLiquibaseConfiguration {
 
-		return liquibase;
+		private final LiquibaseProperties properties;
+
+		@Autowired
+		public DynamicExtensionLiquibaseConfiguration(LiquibaseProperties properties) {
+			this.properties = properties;
+		}
+
+		@Bean
+		public SpringLiquibase liquibase(DataSource dataSource, @Value("${spring.liquibase.change-log}") String changeLog, CurrentConfigurationReader currentConfigurationReader) {
+			ExtensionCapableSpringLiquibase liquibase = new ExtensionCapableSpringLiquibase();
+
+			liquibase.setChangeLog(this.properties.getChangeLog());
+			liquibase.setContexts(this.properties.getContexts());
+			liquibase.setDefaultSchema(this.properties.getDefaultSchema());
+			liquibase.setLiquibaseSchema(this.properties.getLiquibaseSchema());
+			liquibase.setLiquibaseTablespace(this.properties.getLiquibaseTablespace());
+			liquibase.setDatabaseChangeLogTable(this.properties.getDatabaseChangeLogTable());
+			liquibase.setDatabaseChangeLogLockTable(this.properties.getDatabaseChangeLogLockTable());
+			liquibase.setDropFirst(this.properties.isDropFirst());
+			liquibase.setShouldRun(this.properties.isEnabled());
+			liquibase.setLabels(this.properties.getLabels());
+			liquibase.setChangeLogParameters(this.properties.getParameters());
+			liquibase.setRollbackFile(this.properties.getRollbackFile());
+			liquibase.setTestRollbackOnUpdate(this.properties.isTestRollbackOnUpdate());
+
+			liquibase.setDataSource(dataSource);
+			liquibase.setExtensions(currentConfigurationReader.getCurrentExtensions());
+
+			return liquibase;
+		}
 	}
+
 
 }
